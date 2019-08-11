55d9011ea5b9a454698cfd7bb615a550049ce713
Gwj15827934484 <53966857+Gwj15827934484@users.noreply.github.com>
2019-08-11 14:38:16 +0800

    Update GithubDay1ApplicationTests.java

--- src/test/java/com/baizhi/GithubDay1ApplicationTests.java
+++ src/test/java/com/baizhi/GithubDay1ApplicationTests.java
@@ -12,6 +12,7 @@ public class GithubDay1ApplicationTests {
     @Test
     public void contextLoads() {
         System.out.println("=====1=====");
+        System.out.println("=====2=====");
     }
 
 }
