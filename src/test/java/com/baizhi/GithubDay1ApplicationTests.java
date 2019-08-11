c28983dc1e748b0742f1845ee3982a7d128dc951
Gwj15827934484 <53966857+Gwj15827934484@users.noreply.github.com>
2019-08-11 15:16:39 +0800

    Update GithubDay1ApplicationTests.java

--- src/test/java/com/baizhi/GithubDay1ApplicationTests.java
+++ src/test/java/com/baizhi/GithubDay1ApplicationTests.java
@@ -19,5 +19,9 @@ public class GithubDay1ApplicationTests {
         System.out.println("我是小白");
 
     }
+    public void TestBanner(){
+        String str = "hello";
+        System.out.println(str);
+    }
 
 }
