a29a07aed400d8b8e51a12070635710671f92808
Your Name <email@example.com>
2019-08-11 14:19:31 +0800

    第一次創建github_day1基礎的項目環境

---
+++ src/main/java/com/baizhi/GithubDay1Application.java
@@ -0,0 +1,13 @@
+package com.baizhi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubDay1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubDay1Application.class, args);
+    }
+
+}
